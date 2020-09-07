744e8cfec984e165574fa0820a9128c195911acd
Angeleen MCDONNELL Wilson <angeleenmc@gmail.com>
2020-09-07 04:03:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -24,7 +24,9 @@ function calculatesFarePrice(start, end) {
   if (distance <= 400) {
     return 0;
   } else if (distance > 400 && <= 2000) {
-    return (distance - 400)
+    return distance - 400 * .02;
+  } else if (distance > 2000 && < 2500) {
+    return distance - 400 + 45;
   }
   }
 }
