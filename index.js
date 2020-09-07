f5290e14a8e63b32fe650d252f382a9ac7ed807f
Angeleen MCDONNELL Wilson <angeleenmc@gmail.com>
2020-09-07 03:38:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 function distanceFROMHqInBlocks(block) {
   if (block >= 42) {
     return -42;
-    } else if (block < 42)
+    } else if (block < 42) {
+      return 42 - block;
+    }
   }
 
 function distanceFromHqInFeet() {
