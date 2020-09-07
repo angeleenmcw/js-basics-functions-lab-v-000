16e52e598ddd3063830ebbd5a496aa4d63991da2
Angeleen MCDONNELL Wilson <angeleenmc@gmail.com>
2020-09-07 03:24:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-function distanceFROMHqInBlocks(blocks, location) {
+function distanceFROMHqInBlocks() {
   let blocks = 1;
   let location = "below 42nd Street";
-  console.log(`The distance is ${blocks} located ${location}');
+  console.log(`The distance is ${blocks} located ${location}`);
   
+}
+
+function distanceFromHqInFeet() {
+  let feet =
 }
